ae5329c5c6b8752e5948c221881d54671b3b1c73
Michael Aubrey <mcaubrey511@gmail.com>
2019-02-22 02:27:19 +0000

    first steps for php formatting

--- c9/start.sh
+++ c9/start.sh
@@ -7,6 +7,7 @@ if [ -z $C9_PASSWORD ]
 then
     export C9_PASSWORD=$C9_PASS
 fi
+
 sudo a2enmod rewrite
 sudo service apache2 start
 sudo mkdir $C9_WORKSPACE
@@ -19,6 +20,15 @@ fi
 if [ -z $C9_USER ] && [ -n $C9_PASS ]
 then
     echo 'Authorization mode cannot be started without specifying a username ($C9_USER).'
+fi
+
+if [ ! -f $C9_WORKSPACE/.c9/project.settings ]
+then 
+mkdir $C9_WORKSPACE/.c9
+cp /cloud9/project.settings $C9_WORKSPACE/.c9/project.settings
+echo 'Loaded default "project.settings" file.'
+else
+echo '"project.settings" file detected. Using existing file.'
 fi
 
 node /cloud9/server.js --listen 0.0.0.0 --port 8080 -w $C9_WORKSPACE --auth $C9_USER:$C9_PASS $C9_EXTRA
